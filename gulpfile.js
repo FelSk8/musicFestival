ec9c73609c11e61836d82462cb4a728c7a18e583
FelSk8 <felipeguzman20@gmail.com>
2024-03-30 22:25:53 -0300

    nuevos cosas gulp

--- gulpfile.js
+++ gulpfile.js
@@ -8,13 +8,18 @@
 // exports.tarea = tarea; // llamar una función con nodejs
 
 
-const { src, dest, watch} = require("gulp"); // Importa funciones de Gulp para manejar archivos y tareas de automatización
-
+const { src, dest, watch, parallel } = require("gulp"); // Importa funciones de Gulp para manejar archivos y tareas de automatización
+// CSS
 // Importa el paquete gulp-sass para compilar archivos Sass a CSS
 const sass = require('gulp-sass')(require('sass'));
-
 // Importa el paquete gulp-plumber
 const plumber = require('gulp-plumber');
+
+// IMAGENES 
+const cache = require('gulp-cache');
+const imagemin = require('gulp-imagemin');
+const webp = require('gulp-webp');
+const avif = require('gulp-avif');
 
 // Definición de la función para compilar archivos Sass a CSS
 function css(done){
@@ -32,6 +37,40 @@ function css(done){
     // Callback que avisa a gulp cuando la función ha terminado
     done();
 }
+function imagenes( done ) {
+    const opciones = {
+        optimizationLevel: 3
+    }
+
+    src('src/img/**/*.{png,jpg}')
+    .pipe( cache( imagemin(opciones)))
+    .pipe(dest('build/img'))
+    done();
+}
+
+function versionWebp( done ) {
+
+    const opciones = {
+        quality: 50
+    };
+
+    src('src/img/**/*.{png,jpg}')
+        .pipe( webp(opciones) )
+        .pipe( dest('build/img') )
+    done();
+}
+
+function versionAvif( done ) {
+
+    const opciones = {
+        quality: 50
+    };
+
+    src('src/img/**/*.{png,jpg}')
+        .pipe( avif(opciones) )
+        .pipe( dest('build/img') )
+    done();
+}
 
 // Definición de la función para observar cambios en archivos Sass y ejecutar la tarea de compilación
 function dev(done){
@@ -44,4 +83,8 @@ function dev(done){
 
 // Exporta las funciones css y dev para que estén disponibles para ser llamadas desde la línea de comandos de Gulp
 exports.css = css;
-exports.dev = dev;
+exports.imagenes = imagenes;
+exports.versionWebp = versionWebp;
+exports.versionAvif = versionAvif;
+exports.dev = parallel( imagenes, versionWebp, versionAvif, dev) ;
+
