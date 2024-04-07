499667fe444a2ec49ddcc647ecb762948c915772
FelSk8 <felipeguzman20@gmail.com>
2024-04-07 17:16:29 -0400

    fin proyecto

--- gulpfile.js
+++ gulpfile.js
@@ -14,6 +14,13 @@ const { src, dest, watch, parallel } = require("gulp"); // Importa funciones de 
 const sass = require('gulp-sass')(require('sass'));
 // Importa el paquete gulp-plumber
 const plumber = require('gulp-plumber');
+const autoprefixer = require('autoprefixer');
+const cssnano = require('cssnano');
+const postcss = require('gulp-postcss');
+const sourcemaps = require('gulp-sourcemaps');
+
+// Javascript
+const terser = require('gulp-terser-js');
 
 // IMAGENES 
 const cache = require('gulp-cache');
@@ -23,14 +30,16 @@ const avif = require('gulp-avif');
 
 // Definición de la función para compilar archivos Sass a CSS
 function css(done){
-    
     // Identifica el archivo Sass de entrada
     src('src/scss/**/*.scss')
+        .pipe(sourcemaps.init())
           // Usa plumber para prevenir que la ejecución se detenga en caso de errores
           .pipe(plumber())
         // Compila el archivo Sass a CSS
         .pipe( sass() )
-       
+
+        .pipe( postcss([ autoprefixer(), cssnano() ]))
+        .pipe(sourcemaps.write('.'))
         // Almacena el archivo CSS resultante en la carpeta de destino
         .pipe( dest('build/css'));
 
@@ -72,19 +81,30 @@ function versionAvif( done ) {
     done();
 }
 
+function javascript(done) {
+    src('src/js/**/*.js')
+    .pipe(sourcemaps.init())
+    .pipe( terser())
+    .pipe(sourcemaps.write('.'))
+    .pipe(dest('build/js'));
+
+    done();
+}
+
 // Definición de la función para observar cambios en archivos Sass y ejecutar la tarea de compilación
 function dev(done){
     // Observa cambios en el archivo Sass y ejecuta la función css() cuando se producen cambios
     watch('src/scss/**/*.scss', css)
-
+    watch('src/js/**/*.js', javascript)
     // Callback que avisa a gulp cuando la función ha terminado
     done();
 }
 
 // Exporta las funciones css y dev para que estén disponibles para ser llamadas desde la línea de comandos de Gulp
 exports.css = css;
+exports.js = javascript;
 exports.imagenes = imagenes;
 exports.versionWebp = versionWebp;
 exports.versionAvif = versionAvif;
-exports.dev = parallel( imagenes, versionWebp, versionAvif, dev) ;
+exports.dev = parallel( imagenes, versionWebp, versionAvif, javascript, dev) ;
 
